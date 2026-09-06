d8365b31f1ceba5775696eac4d3fd7244fff4bff
sgrinan <102607477+sgrinan@users.noreply.github.com>
2026-09-06 17:06:45 +0200

    refactor(main): adapt style

--- cmd/signaldock/main.go
+++ cmd/signaldock/main.go
@@ -14,17 +14,25 @@ import (
 	"github.com/sgrinan/signaldock/internal/web"
 )
 
+const (
+	defaultPort     = "8080"
+	shutdownTimeout = 10 * time.Second
+)
+
 func main() {
 	port := os.Getenv("SIGNALDOCK_PORT")
 	if port == "" {
-		port = "8080"
+		port = defaultPort
 	}
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(
+		slog.NewTextHandler(os.Stdout, nil),
+	)
 
 	store := endpoint.NewStore()
+	service := endpoint.NewService(store)
 
-	handler, err := web.NewHandler(store, logger)
+	handler, err := web.NewHandler(service, logger)
 	if err != nil {
 		logger.Error("failed to create HTTP handler", "error", err)
 		os.Exit(1)
@@ -35,30 +43,52 @@ func main() {
 		Handler:           handler,
 		ReadHeaderTimeout: 5 * time.Second,
 		ReadTimeout:       10 * time.Second,
-		WriteTimeout:      10 * time.Second,
+		WriteTimeout:      30 * time.Second,
 		IdleTimeout:       60 * time.Second,
 	}
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-
-	logger.Info("starting SignalDock", "port", port)
+	serverErr := make(chan error, 1)
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logger.Error("HTTP server stopped", "error", err)
+		logger.Info("starting SignalDock", "port", port)
+
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+			return
 		}
+
+		serverErr <- nil
 	}()
 
-	<-shutdown
+	shutdown := make(chan os.Signal, 1)
 
-	logger.Info("shutting down SignalDock")
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(shutdown)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	select {
+	case sig := <-shutdown:
+		logger.Info("shutting down SignalDock", "signal", sig.String())
+
+	case err := <-serverErr:
+		if err != nil {
+			logger.Error("failed to start HTTP server", "error", err)
+			os.Exit(1)
+		}
+
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		shutdownTimeout,
+	)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
 		logger.Error("failed to shutdown HTTP server", "error", err)
 		os.Exit(1)
 	}
+
+	logger.Info("SignalDock stopped")
 }
