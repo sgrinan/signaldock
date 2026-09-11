54ecb079ba0bae08fd28f54e4aec66ece140ef96
sgrinan <102607477+sgrinan@users.noreply.github.com>
2026-09-11 06:20:15 +0200

    refactor: separate application runner

--- cmd/signaldock/main.go
+++ cmd/signaldock/main.go
@@ -1,177 +1,17 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"flag"
 	"log/slog"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"uuid"
-
-	"github.com/sgrinan/signaldock/internal/auth"
-	"github.com/sgrinan/signaldock/internal/database"
-	"github.com/sgrinan/signaldock/internal/endpoint"
-	"github.com/sgrinan/signaldock/internal/session"
-	"github.com/sgrinan/signaldock/internal/user"
-	"github.com/sgrinan/signaldock/internal/web"
-)
-
-const (
-	defaultPort     = "8080"
-	shutdownTimeout = 10 * time.Second
 )
 
 func main() {
-	showVersion := flag.Bool("version", false, "print version information")
-	flag.Parse()
-
-	if *showVersion {
-		printVersion()
-		return
-	}
-
-	port := os.Getenv("SIGNALDOCK_PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	logger := slog.New(
 		slog.NewTextHandler(os.Stdout, nil),
 	)
 
-	databaseURL := os.Getenv("SIGNALDOCK_DATABASE_URL")
-	if databaseURL == "" {
-		logger.Error("SIGNALDOCK_DATABASE_URL is required")
+	if err := run(logger); err != nil {
+		logger.Error("SignalDock failed", "error", err)
 		os.Exit(1)
 	}
-
-	ctx := context.Background()
-
-	pool, err := database.NewPostgresPool(ctx, databaseURL)
-	if err != nil {
-		logger.Error("failed to connect to PostgreSQL", "error", err)
-		os.Exit(1)
-	}
-	defer pool.Close()
-
-	if err := database.EnsureSchema(ctx, pool); err != nil {
-		logger.Error("failed to apply database migrations", "error", err)
-		os.Exit(1)
-	}
-
-	endpointRepository := endpoint.NewRepository(pool)
-	endpointChecks := endpoint.NewCheckStore()
-	endpointService := endpoint.NewService(endpointRepository, endpointChecks)
-
-	userRepository := user.NewRepository(pool)
-
-	users, err := userRepository.List()
-	if err != nil {
-		logger.Error("failed to load users", "error", err)
-		os.Exit(1)
-	}
-
-	if len(users) == 0 {
-		username := os.Getenv("SIGNALDOCK_ADMIN_USERNAME")
-		password := os.Getenv("SIGNALDOCK_ADMIN_PASSWORD")
-
-		if username == "" || password == "" {
-			logger.Error("initial admin credentials are required")
-			os.Exit(1)
-		}
-
-		admin := user.User{
-			ID:           uuid.NewV7(),
-			Username:     username,
-			PasswordHash: auth.HashPassword(password),
-			Role:         user.RoleAdmin,
-		}
-
-		if err := userRepository.Insert(admin); err != nil {
-			logger.Error("failed to create initial admin", "error", err)
-			os.Exit(1)
-		}
-
-		logger.Info("initial admin created", "username", username)
-	}
-
-	sessionStore := session.NewRepository(pool)
-	sessionService := session.NewService(sessionStore)
-
-	endpoints, err := endpointService.List()
-	if err != nil {
-		logger.Error("failed to load endpoints", "error", err)
-		os.Exit(1)
-	}
-
-	for _, ep := range endpoints {
-		if _, err := endpointService.Refresh(ep.ID); err != nil {
-			logger.Warn("failed to refresh endpoint on startup", "endpoint_id", ep.ID, "url", ep.URL, "error", err)
-		}
-	}
-
-	handler, err := web.NewHandler(endpointService, userRepository, sessionService, logger)
-	if err != nil {
-		logger.Error("failed to create HTTP handler", "error", err)
-		os.Exit(1)
-	}
-
-	server := &http.Server{
-		Addr:              ":" + port,
-		Handler:           handler,
-		ReadHeaderTimeout: 5 * time.Second,
-		ReadTimeout:       10 * time.Second,
-		WriteTimeout:      30 * time.Second,
-		IdleTimeout:       60 * time.Second,
-	}
-
-	serverErr := make(chan error, 1)
-
-	go func() {
-		logger.Info("starting SignalDock", "port", port)
-
-		err := server.ListenAndServe()
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			serverErr <- err
-			return
-		}
-
-		serverErr <- nil
-	}()
-
-	shutdown := make(chan os.Signal, 1)
-
-	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-	defer signal.Stop(shutdown)
-
-	select {
-	case sig := <-shutdown:
-		logger.Info("shutting down SignalDock", "signal", sig.String())
-
-	case err := <-serverErr:
-		if err != nil {
-			logger.Error("failed to start HTTP server", "error", err)
-			os.Exit(1)
-		}
-
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		shutdownTimeout,
-	)
-	defer cancel()
-
-	if err := server.Shutdown(ctx); err != nil {
-		logger.Error("failed to shutdown HTTP server", "error", err)
-		os.Exit(1)
-	}
-
-	logger.Info("SignalDock stopped")
 }
---
+++ cmd/signaldock/run.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"uuid"
+
+	"github.com/sgrinan/signaldock/internal/auth"
+	"github.com/sgrinan/signaldock/internal/database"
+	"github.com/sgrinan/signaldock/internal/endpoint"
+	"github.com/sgrinan/signaldock/internal/session"
+	"github.com/sgrinan/signaldock/internal/user"
+	"github.com/sgrinan/signaldock/internal/web"
+)
+
+const (
+	defaultPort     = "8080"
+	shutdownTimeout = 10 * time.Second
+)
+
+func run(logger *slog.Logger) error {
+	showVersion := flag.Bool("version", false, "print version information")
+	flag.Parse()
+
+	if *showVersion {
+		printVersion()
+		return nil
+	}
+
+	port := os.Getenv("SIGNALDOCK_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	databaseURL := os.Getenv("SIGNALDOCK_DATABASE_URL")
+	if databaseURL == "" {
+		return errors.New("SIGNALDOCK_DATABASE_URL is required")
+	}
+
+	ctx := context.Background()
+
+	pool, err := database.NewPostgresPool(ctx, databaseURL)
+	if err != nil {
+		return fmt.Errorf("connect to PostgreSQL: %w", err)
+	}
+	defer pool.Close()
+
+	if err := database.EnsureSchema(ctx, pool); err != nil {
+		return fmt.Errorf("apply database migrations: %w", err)
+	}
+
+	endpointRepository := endpoint.NewRepository(pool)
+	endpointChecks := endpoint.NewCheckStore()
+	endpointService := endpoint.NewService(endpointRepository, endpointChecks)
+
+	userRepository := user.NewRepository(pool)
+
+	users, err := userRepository.List()
+	if err != nil {
+		return fmt.Errorf("load users: %w", err)
+	}
+
+	if len(users) == 0 {
+		username := os.Getenv("SIGNALDOCK_ADMIN_USERNAME")
+		password := os.Getenv("SIGNALDOCK_ADMIN_PASSWORD")
+
+		if username == "" || password == "" {
+			return errors.New("initial admin credentials are required")
+		}
+
+		admin := user.User{
+			ID:           uuid.NewV7(),
+			Username:     username,
+			PasswordHash: auth.HashPassword(password),
+			Role:         user.RoleAdmin,
+		}
+
+		if err := userRepository.Insert(admin); err != nil {
+			return fmt.Errorf("create initial admin: %w", err)
+		}
+
+		logger.Info("initial admin created", "username", username)
+	}
+
+	sessionRepository := session.NewRepository(pool)
+	sessionService := session.NewService(sessionRepository)
+
+	endpoints, err := endpointService.List()
+	if err != nil {
+		return fmt.Errorf("load endpoints: %w", err)
+	}
+
+	for _, ep := range endpoints {
+		if _, err := endpointService.Refresh(ep.ID); err != nil {
+			logger.Warn("failed to refresh endpoint on startup", "endpoint_id", ep.ID, "url", ep.URL, "error", err)
+		}
+	}
+
+	handler, err := web.NewHandler(endpointService, userRepository, sessionService, logger)
+	if err != nil {
+		return fmt.Errorf("create HTTP handler: %w", err)
+	}
+
+	server := &http.Server{
+		Addr:              ":" + port,
+		Handler:           handler,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	serverErr := make(chan error, 1)
+
+	go func() {
+		logger.Info("starting SignalDock", "port", port)
+
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+			return
+		}
+
+		serverErr <- nil
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(shutdown)
+
+	select {
+	case sig := <-shutdown:
+		logger.Info("shutting down SignalDock", "signal", sig.String())
+
+	case err := <-serverErr:
+		if err != nil {
+			return fmt.Errorf("start HTTP server: %w", err)
+		}
+
+		return nil
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		return fmt.Errorf("shutdown HTTP server: %w", err)
+	}
+
+	logger.Info("SignalDock stopped")
+
+	return nil
+}
